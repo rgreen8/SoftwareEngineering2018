97aaa8057f5a85c95bec243aa4ac15264db36ed5
Ryan Green <rgreen8@nd.edu>
2018-09-02 19:18:17 -0400

    1 and 2

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,18 @@
 package edu.nd.se2018.homework.hwk1;
-
+import java.util.*;
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		int sum = 0;
+		HashSet<Integer> tracker = new HashSet<Integer>();
+		for (int it=0; it<numbers.length; it++) {
+			if(!tracker.contains(numbers[it])){
+				tracker.add(numbers[it]);
+				sum = sum + numbers[it];
+			}
+        }
+		return sum;	
 	}
 }
--- src/edu/nd/se2018/homework/hwk1/Question2.java
+++ src/edu/nd/se2018/homework/hwk1/Question2.java
@@ -1,10 +1,47 @@
 package edu.nd.se2018.homework.hwk1;
-
+import java.util.*;
 public class Question2 {
 
 	public Question2(){}
 	
 	public String getMostFrequentWord(String input, String stopwords){
-		return "";
+		HashMap<String, Integer> inputTrack = new HashMap<String, Integer>();
+		HashSet<String> stops = new HashSet<String>();
+		//put stop words in a set
+		for (String word : stopwords.split(" ")) {
+			stops.add(word);
+		}
+		//iterate through string
+		for (String word : input.split(" ")) {
+			if(!stops.contains(word)) { //valid word
+				//check if already tracked
+				//if so add one to total
+				//else enter it as 1
+				if(inputTrack.containsKey(word)) {
+					inputTrack.put(word, inputTrack.get(word) + 1);
+				}else {
+					inputTrack.put(word, 1);
+				}
+			}
+		}
+		//find highest times
+		String maxInput = "";
+		int maxValue = 0;
+		for(String word : inputTrack.keySet()){
+			if(inputTrack.get(word) == maxValue) {
+				maxInput = "";
+				maxValue = inputTrack.get(word);
+			}
+			if(inputTrack.get(word) > maxValue) {
+				maxInput = word;
+				maxValue = inputTrack.get(word);
+			}
+		}
+		//return highest time
+		System.out.println(maxInput);
+		if(maxInput == "") {
+			return null;
+		}
+		return maxInput;
 	}
 }
